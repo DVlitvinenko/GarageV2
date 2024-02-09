495ce8b6d12e795f57bb8ee0e6617485ec7db6d6
DVlitvinenko <124286708+DVlitvinenko@users.noreply.github.com>
2024-02-09 14:19:54 +0300

    https

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\APIController;
+use Illuminate\Support\Facades\URL;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +14,7 @@ use App\Http\Controllers\APIController;
 |
 */
 
+URL::forceScheme('https');
 // Route::get('/', function () {
 //     return redirect()->route('home');
 // });
