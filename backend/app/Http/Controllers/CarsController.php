6220e20cd1aea930c68866677f1435d03d8b3a2a
DVlitvinenko <124286708+DVlitvinenko@users.noreply.github.com>
2024-02-14 14:43:20 +0300

    fix cars/ api method

--- backend/app/Http/Controllers/CarsController.php
+++ backend/app/Http/Controllers/CarsController.php
@@ -29,7 +29,7 @@ class CarsController extends Controller
      * Получение списка автомобилей с учетом фильтров (аутентифицированный запрос)
      *
      * @OA\Get(
-     *     path="/cars",
+     *     path="/cars/filter",
      *     operationId="getCars",
      *     summary="Получение списка автомобилей с учетом фильтров (аутентифицированный запрос)",
      *     tags={"Cars"},
